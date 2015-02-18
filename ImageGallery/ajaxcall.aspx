cdcad26394fffe9a862611b08171196ffe31e637
rjadhav <rjadhav@ac1760f5-c309-464f-b585-17c111484dec>
2015-02-18 04:20:18 +0000

    added Image Gallery
    git-svn-id: http://code-rovia.travelnxt.com:8080/svn/Users/Rahul_J/ImageGallery/trunk@1523 ac1760f5-c309-464f-b585-17c111484dec

---
+++ ImageGallery/ajaxcall.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#"  %>
+<%@ Import Namespace="System.Data" %>
+<%@ Import Namespace="System.Data.SqlClient" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <script runat ="server">
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          //  Response.Write("Hello");
+            string data = Request.QueryString["src"];
+           // Response.Write(data);
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\rjadhav\Documents\Assignment1.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+
+            cmd.CommandText = "select imagename,imagepath,uploaddate,imagesize,imagetype from imagedata where ImagePath='" + data + "'";
+
+            cmd.Connection = con;
+            con.Open();
+           
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.HasRows)
+            {
+                
+                while (dr.Read())
+                {
+                    
+                    string name = dr["ImageName"].ToString();
+                    string path = dr["ImagePath"].ToString();
+                    string created = dr["UploadDate"].ToString();
+                    string size = dr["ImageSize"].ToString();
+                    string type = dr["ImageType"].ToString();
+
+                    Response.Write("<hr/><div class=content><b> Name :</b> " + name + "<br/>");
+                    Response.Write("<b>Path :</b> " + path + "<br/>");
+                    Response.Write("<b>Upload Date : </b>" + created + "<br/>");
+                    Response.Write("<b>Image Size : </b>" + size + "<br/>");
+                    Response.Write("<b>Image Type : </b>" + type + "<br/></div>");
+                }
+            }
+            con.Close();
+            
+        }
+    
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ImageGallery/index.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ImageGallery
+{
+    public partial class index : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            FileStream fs2 = new FileStream(Server.MapPath("ImageName.txt"), FileMode.Open,FileAccess.Read);
+
+            StreamReader sr = new StreamReader(fs2);
+            String data = sr.ReadToEnd();
+
+            string[] url = data.Split('|');
+
+
+            for (int i = 0; i < url.Length; i++)
+            {
+
+                HtmlImage obj = new HtmlImage();
+                obj.Src = url[i];
+                obj.Visible = true;
+                obj.Attributes.Add("class", "image");
+                divContainer.Controls.Add(obj);
+
+
+            }
+            sr.Close();
+            fs2.Close();
+           
+        }
+
+
+
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileUpload1.SaveAs(Server.MapPath("~//Uploads//" + FileUpload1.FileName));
+
+                FileStream fs1 = new FileStream(Server.MapPath("ImageName.txt"), FileMode.Append, FileAccess.Write);
+
+                StreamWriter sw = new StreamWriter(fs1);
+                string url = "~//Uploads//" + FileUpload1.FileName;
+                sw.WriteLine(url);
+
+                sw.Write("|");
+                sw.Close();
+                fs1.Close();
+
+
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\rjadhav\Documents\Assignment1.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.Text;
+                String name = FileUpload1.FileName;
+                String type = FileUpload1.PostedFile.ContentType;
+                int size = FileUpload1.PostedFile.ContentLength;
+                string url1 = "Uploads/" + FileUpload1.FileName;
+                cmd.CommandText = "insert into ImageData values ('" + name + "','" + url1 + "','" + DateTime.Now.ToShortDateString() + "','" + size + "','" + type + "')";
+
+                cmd.Connection = con;
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                Response.Redirect("~//index.aspx");
+
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
